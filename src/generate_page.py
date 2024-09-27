6449f7841ecdc88a7267ff201140063fd39dca38
Szeri <szeri323@gmail.com>
2024-09-27 12:03:22 +0200

    update main to use relative path, fix recursion in page generator, update page generator to set styles depending on path depth

--- src/generate_page.py
+++ src/generate_page.py
@@ -11,15 +11,17 @@ def extract_title(markdown):
             return line[2:]
     raise ValueError("No title found")
 
-def generate_page_recursive(content_path, static_path, template_path, dest_path): 
+def generate_page_recursive(ansolute_content_path, content_path, static_path, template_path, dest_path): 
     markdown = ""
     template = ""
-    with open(template_path) as t:
-        template = t.read()
-        t.close()
     for file_md in os.listdir(content_path):
+
+        with open(template_path) as t:
+            template = t.read()
+            t.close()
+
         if os.path.isfile(content_path + '/' + file_md):
-            with open(content_path+"/"+'index.md') as f:
+            with open(content_path+"/"+file_md) as f:
                 markdown = f.read()
                 f.close()
 
@@ -27,11 +29,23 @@ def generate_page_recursive(content_path, static_path, template_path, dest_path)
             html = node.to_html()
             
             title = extract_title(markdown)
+
+            depth_of_file = content_path.replace(ansolute_content_path, '')
+
+            counter = depth_of_file.split('/')
+            if len(counter) == 1:
+                template = template.replace("{{ Style }}", './index.css')
+            else :
+                template = template.replace("{{ Style }}", '../' * (len(counter) - 1) + 'index.css')
+
             template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
-        
-            with open(dest_path + '/' + 'index.html', 'w+') as f:
+
+
+            file_html = file_md.replace('.md','.html')
+
+            with open(dest_path + '/' + file_html, 'w+') as f:
                 print(template, file=f)
                 f.close()
         else:
             os.mkdir(dest_path + '/' + file_md)
-            generate_page_recursive(content_path + '/' + file_md, static_path, template_path, dest_path + '/' + file_md)
+            generate_page_recursive(ansolute_content_path, content_path + '/' + file_md, static_path, template_path, dest_path + '/' + file_md)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 from copy_from_to import copy_from_to_recursive
 from generate_page import generate_page_recursive
+from sys import platform
+import os
 
 def main():
-    content_path = '/home/szeri/git/boot.dev/static-site-generator/content'
-    static_path = '/home/szeri/git/boot.dev/static-site-generator/static'
-    template_path = '/home/szeri/git/boot.dev/static-site-generator/html_templates/template.html'
-    dest_path = '/home/szeri/git/boot.dev/static-site-generator/public'
+    path_sign = '/'
+    if platform == "win32":
+        path_sign = '\\'
+
+    print(platform)
+    print(os.getcwd().replace(path_sign+'src', path_sign+'content'))
+    content_path = os.getcwd().replace(path_sign+'src', path_sign+'content')
+    static_path = os.getcwd().replace(path_sign+'src', path_sign+'static')
+    template_path = os.getcwd().replace(path_sign+'src', path_sign+'html_templates' + path_sign + 'template.html')
+    dest_path = os.getcwd().replace(path_sign+'src', path_sign+'public')
     print(f'Generating page from {content_path} to {dest_path} using {template_path}')
     copy_from_to_recursive(static_path, dest_path)
-    generate_page_recursive(content_path, static_path, template_path, dest_path)
+    generate_page_recursive(content_path, content_path, static_path, template_path, dest_path)
     print(f'Generating done')
     
 main()
